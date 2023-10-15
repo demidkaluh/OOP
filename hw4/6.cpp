2d3c9d328684c2d2f4dd39661c2431e83c464f17
Демид Калюх <111245496+demidkaluh@users.noreply.github.com>
2023-10-15 20:06:33 +0300

    Add files via upload

--- hw4/6.cpp
+++ hw4/6.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <array>
 #include <string>
+#include <vector>
+#include <span>
 #include <utility>
 #include <cmath>
+
 #include "container_cout.h"
 
-using std::cout, std::endl;
+using std::cout, std::endl, std::string, std::vector,
+        std::string_view, std::array, std::pair;
 
-
-template<typename T>
-std::vector< std::pair<T, T> > pairing(std::vector<T> v)
+template <typename T>
+auto pairing(const T& C)
 {
-    std::vector< std::pair<T, T> > res;
-    size_t sz = v.size();
+    using pairtype = std::pair<typename T::value_type, typename T::value_type>;
+	using rtype = std::vector<pairtype>;
 
-    for (size_t i = 0; i < ceil(sz / 2.); i++)
-        res.push_back({0, 0});
 
-    for (size_t i = 0; i < sz; i++)
+    pairtype p;
+	rtype res;
+
+    for (int i = 0; i < (std::ceil(C.size() / 2.)); i++)
     {
-        if (i % 2 == 0)
-            res[i / 2].first = v[i];
-        else
-            res[i / 2].second = v[i];
+        res.push_back(p);        
     }
 
-    return res;
+    for (int i = 0; i < C.size(); i++)
+    {
+        if (i % 2 == 0)
+            res[i / 2].first = C[i];
+        else
+            res[i / 2].second = C[i];
+    }
+
+	return res;
 }
 
 
@@ -34,16 +41,14 @@ int main()
 {
     std::vector <int> v {10, 20, 30, 40, 50, 60};
     cout << pairing(v) << endl;
-    std::vector <std::string> v2 = {"m", "n", "1"};
-    cout << pairing(v2);
+    
+
+    std::array<std::string, 7> a {"cat", "dog", "mouse", "elephant", "tiget", "axolotl", "wolf"};
+    cout << pairing(a) << endl;
 
 
-    //std::array<std::string, 7> a {"cat", "dog", "mouse", "elephant", "tiget", "axolotl", "wolf"};
-    //cout << pairing(a) << endl;
-
-
-    //std::string s {"Cats and dogs!"};
-    //cout << pairing(s) << endl;
+    std::string s {"Cats and dogs!"};
+    cout << pairing(s) << endl;
 }
 
 
